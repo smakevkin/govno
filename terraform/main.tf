184de2e64533e76696096c914d5137d1c7632720
smakevkin <smakevkin@mail.ru>
2023-11-25 18:22:59 +1000

    kekw

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,36 @@ provider "yandex" {
   folder_id                = "b1g43aq7nnnac7rsogvm"
   zone                     = "ru-central1-a"
 }
+resource "yandex_compute_instance" "default" {
+  name        = "test"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "image_id"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.foo.id}"
+  }
+
+  metadata = {
+    foo      = "bar"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "foo" {}
+
+resource "yandex_vpc_subnet" "foo" {
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.foo.id}"
+  v4_cidr_blocks = ["10.5.0.0/24"]
+}
