cc21ed9bf8db85938d296e0bf16e8c23e929af15
smakevkin <smakevkin@mail.ru>
2023-11-25 18:02:41 +1000

    terra file

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,5 +8,8 @@ terraform {
 }
 
 provider "yandex" {
-  zone = "ru-central1-a"
+  token                    = "y0_AgAAAAAJ89LzAATuwQAAAADXw9Jg9c02CFcWRByVNmiLPNOJSPw0xPA"
+  cloud_id                 = "b1gc7onttnadnivcrdpf"
+  folder_id                = "b1g43aq7nnnac7rsogvm"
+  zone                     = "ru-central1-a"
 }
